ce27b2ea7bb8316a12ac63221654997c6db7bf2a
Mohitur Rahman Zidan <mohit.zidane73@gmail.com>
2026-07-08 21:44:31 +0200

    Restore project to Starting_Rock animation point

--- src/formation/stage-controller.js
+++ src/formation/stage-controller.js
@@ -69,13 +69,13 @@ export function createGelifluctionStageController({ config, THREE, updateHud }) 
       startStageFourAnimation(-1);
     } else if (previousStage === 4 && targetStage === 3) {
       startCrossfade(
-        [instance.nodes.Surrounding_Rocks, instance.nodes.Slope],
+        [instance.nodes.Starting_Rock, instance.nodes.Surrounding_Rocks, instance.nodes.Slope],
         [instance.nodes.Earth_Crust_Right, instance.nodes["3rd Stage Rock"]]
       );
     } else if (previousStage === 3 && targetStage === 4) {
       startCrossfade(
         [instance.nodes.Earth_Crust_Right, instance.nodes["3rd Stage Rock"]],
-        [instance.nodes.Surrounding_Rocks, instance.nodes.Slope]
+        [instance.nodes.Starting_Rock, instance.nodes.Surrounding_Rocks, instance.nodes.Slope]
       );
     } else if (previousStage === 3 && targetStage === 2) {
       startCrossfade(
@@ -231,6 +231,7 @@ export function createGelifluctionStageController({ config, THREE, updateHud }) 
 
   function managedNodes() {
     return [
+      instance.nodes.Starting_Rock,
       instance.nodes.Surrounding_Rocks,
       instance.nodes.Slope,
       instance.nodes.Earth_Crust_Right,
