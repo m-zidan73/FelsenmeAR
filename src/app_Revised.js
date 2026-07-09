49b8ceb778a8013611ef3f6a65870fafc18cd192
DiegoArandia <diego.e.cardozo@stud.h-da.de>
2026-07-09 10:04:28 +0200

    Revert "sync upstream commit 8ecbdaf: reorder formationSliderUi before stageController, add setPinchPromptVisible/onSubductionPromptVisibleChange, add subductionAction/holdSubductionAnimationAtEnd to stage-controller, add pinchPrompt arrows UI/CSS"
    This reverts commit 3f242d892f329a49602697c713b8fcc141aa4899.

--- src/app_Revised.js
+++ src/app_Revised.js
@@ -47,20 +47,10 @@ import { createDataOverlayController } from "./data-overlay-controller.js";
     setScanPromptVisible
   } = menuUi;
 
-  const formationSliderUi = createFormationSlider({
-    ui,
-    clamp: THREE.MathUtils.clamp,
-    onStepSelected(stepIndex, previousStep) {
-      return requestStageWrapped(stepIndex + 1, previousStep + 1);
-    }
-  });
-  const { initFormationSlider, resetFormationSlider, setPinchPromptVisible } = formationSliderUi;
-
   const stageController = createGelifluctionStageController({
     config: CONFIG,
     THREE,
-    updateHud,
-    onSubductionPromptVisibleChange: setPinchPromptVisible
+    updateHud
   });
   const {
     getCurrentStage,
@@ -93,6 +83,15 @@ import { createDataOverlayController } from "./data-overlay-controller.js";
     }
     return accepted;
   }
+
+  const formationSliderUi = createFormationSlider({
+    ui,
+    clamp: THREE.MathUtils.clamp,
+    onStepSelected(stepIndex, previousStep) {
+      return requestStageWrapped(stepIndex + 1, previousStep + 1);
+    }
+  });
+  const { initFormationSlider, resetFormationSlider } = formationSliderUi;
 
   const sceneController = createSceneController({
     state,
