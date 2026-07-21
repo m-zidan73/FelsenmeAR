d6ebf126da4f9eb885d0540e336eed0dda5de70a
Mohitur Rahman Zidan <mohit.zidane73@gmail.com>
2026-07-21 22:36:50 +0200

    Revert "Pinch icon orientation fix"
    This reverts commit ffe2a234b9a42e7805948f1bed03ff97a2a28e6e.

--- src/app_Revised.js
+++ src/app_Revised.js
@@ -355,13 +355,6 @@ import { PRELOAD_ASSET_URLS, PRELOAD_CACHE_NAME } from "./preload-manifest.js";
     returnToMainMenuPlacement();
   }
 
-  function getActiveTectonicCamera() {
-    if (state.renderer?.xr?.isPresenting && typeof state.renderer.xr.getCamera === "function") {
-      return state.renderer.xr.getCamera(state.camera);
-    }
-    return state.camera;
-  }
-
   function getTectonicPlacementOptions() {
     return {
       scene: state.scene,
@@ -369,7 +362,7 @@ import { PRELOAD_ASSET_URLS, PRELOAD_CACHE_NAME } from "./preload-manifest.js";
       position: state.placementCenter,
       planeHeight: state.planeHeight,
       quaternion: state.formationRoot ? state.formationRoot.quaternion : null,
-      camera: getActiveTectonicCamera()
+      camera: state.camera
     };
   }
 
